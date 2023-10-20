234f0258b868361ce955d274266c0887660a25ab
Joshi <joshua@lipps.de>
2023-10-21 01:01:33 +0200

    added flate.sh

---
+++ flate.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+# de-/inflate files to bypass use of git lfs
+
+if [ ! -e partaa ]; then
+	echo "deflating..."
+	tar -czf total.tar.gz $(ls | grep -v "flate.sh")
+
+	split total.tar.gz -b 40M part
+	rm -r $(ls | grep -v "flate.sh" | grep -v "part")
+else
+	echo "inflating..."
+	cat part* > total.tar.gz
+
+	tar -xzf total.tar.gz
+	rm part*
+fi
